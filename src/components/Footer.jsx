8d594a80c8386c4f63351cc4ff1cbf81f0fa3241
Chrstn67 <chrstn.hmbrt67.com>
2025-06-17 15:34:32 +0200

    MAJ depploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,11 +34,10 @@ function Footer() {
                 1 - Édition du site : En vertu de l'article 6 de la loi n°
                 2004-575 du 21 juin 2004 pour la confiance dans l'économie
                 numérique, il est précisé aux utilisateurs du site internet
-                https://chrstn67.github.io/course-orientation-SAJ-Molsheim/
-                l'identité des différents intervenants dans le cadre de sa
-                réalisation et de son suivi: Propriétaire du site : Christian
-                HUMBERT - Contact :<i>chrstn.hmbrt67@outlook.com</i> - Hébergeur
-                : GitHub
+                https://chrstn67.github.io/courseorientation/ l'identité des
+                différents intervenants dans le cadre de sa réalisation et de
+                son suivi: Propriétaire du site : Christian HUMBERT - Contact :
+                <i>chrstn.hmbrt67@outlook.com</i> - Hébergeur : GitHub
               </p>
 
               <p>
@@ -61,16 +60,15 @@ function Footer() {
                 3 - Limitations de responsabilité : Christian HUMBERT ne pourra
                 être tenu pour responsable des dommages directs et indirects
                 causés au matériel de l'utilisateur, lors de l'accès au site
-                https://chrstn67.github.io/course-orientation-SAJ-Molsheim/.
-                Christian HUMBERT décline toute responsabilité quant à
-                l'utilisation qui pourrait être faite des informations et
-                contenus présents sur
-                https://chrstn67.github.io/course-orientation-SAJ-Molsheim/.
-                Christian HUMBERT s'engage à sécuriser au mieux le site
-                https://chrstn67.github.io/course-orientation-SAJ-Molsheim/,
-                cependant sa responsabilité ne pourra être mise en cause si des
-                données indésirables sont importées et installées sur son site à
-                son insu. Des espaces interactifs (espace contact) sont à la
+                https://chrstn67.github.io/courseorientation/. Christian HUMBERT
+                décline toute responsabilité quant à l'utilisation qui pourrait
+                être faite des informations et contenus présents sur
+                https://chrstn67.github.io/courseorientation/. Christian HUMBERT
+                s'engage à sécuriser au mieux le site
+                https://chrstn67.github.io/courseorientation/, cependant sa
+                responsabilité ne pourra être mise en cause si des données
+                indésirables sont importées et installées sur son site à son
+                insu. Des espaces interactifs (espace contact) sont à la
                 disposition des utilisateurs. Christian HUMBERT se réserve le
                 droit de supprimer, sans mise en demeure préalable, tout contenu
                 déposé dans cet espace qui contreviendrait à la législation
@@ -87,29 +85,28 @@ function Footer() {
                 4 - CNIL et gestion des données personnelles : Conformément aux
                 dispositions de la loi 78-17 du 6 janvier 1978 modifiée,
                 l'utilisateur du
-                sitehttps://chrstn67.github.io/course-orientation-SAJ-Molsheim/
-                dispose d'un droit d'accès, de modification et de suppression
-                des informations collectées. Pour exercer ce droit, envoyez un
+                sitehttps://chrstn67.github.io/courseorientation/ dispose d'un
+                droit d'accès, de modification et de suppression des
+                informations collectées. Pour exercer ce droit, envoyez un
                 message à notre Délégué à la Protection des Données : Christian
                 HUMBERT - chrstn.hmbrt67@outlook.com.
               </p>
 
               <p>
                 5 - Liens hypertextes et cookies : Le site
-                https://chrstn67.github.io/course-orientation-SAJ-Molsheim/
-                contient des liens hypertextes vers d'autres sites et dégage
-                toute responsabilité à propos de ces liens externes ou des liens
-                créés par d'autres sites
-                vershttps://chrstn67.github.io/course-orientation-SAJ-Molsheim/.
+                https://chrstn67.github.io/courseorientation/ contient des liens
+                hypertextes vers d'autres sites et dégage toute responsabilité à
+                propos de ces liens externes ou des liens créés par d'autres
+                sites vershttps://chrstn67.github.io/courseorientation/.
               </p>
 
               <p>
                 6 - Droit applicable et attribution de juridiction : Tout litige
                 en relation avec l'utilisation du site
-                https://chrstn67.github.io/course-orientation-SAJ-Molsheim/ est
-                soumis au droit français. En dehors des cas où la loi ne le
-                permet pas, il est fait attribution exclusive de juridiction aux
-                tribunaux compétents.
+                https://chrstn67.github.io/courseorientation/ est soumis au
+                droit français. En dehors des cas où la loi ne le permet pas, il
+                est fait attribution exclusive de juridiction aux tribunaux
+                compétents.
               </p>
             </div>
             <button
